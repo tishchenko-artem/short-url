cf4339a41a3f9bdf0525fdb65223099ea77abbea
Artem <a.tishchenko.la@gmail.com>
2021-04-27 18:40:58 +0300

    Complete task 1

---
+++ README.md
@@ -0,0 +1,4 @@
+Flask URL Shortener
+
+file task1.sql contains the answer for task:  
+"Написать SQL-запрос, который будет выводить url и description записей, у которых location это Киев или Львов."
--- task1.sql
+++ task1.sql
@@ -13,15 +13,15 @@ CREATE TABLE JOB_TEXT
     DESCRIPTION text
 );
 
-INSERT INTO JOB (location, url, salary) VALUES ('Киев', 'https://', 200);
-INSERT INTO JOB (location, url, salary) VALUES ('Львов', 'http', 400);
-INSERT INTO JOB (location, url, salary) VALUES ('Днепр', 'tcp', 600);
+INSERT INTO JOB (location, url, salary) VALUES ('Київ', 'https://', 200);
+INSERT INTO JOB (location, url, salary) VALUES ('Львів', 'http://', 400);
+INSERT INTO JOB (location, url, salary) VALUES ('Дніпро', 'https://', 600);
 
-INSERT INTO JOB_TEXT  VALUES (1, 'Работа в Киеве');
-INSERT INTO JOB_TEXT  VALUES (2, 'Работа в Львове');
-INSERT INTO JOB_TEXT  VALUES (3, 'Работа в Днепре');
+INSERT INTO JOB_TEXT  VALUES (1, 'Работа в Києві');
+INSERT INTO JOB_TEXT  VALUES (2, 'Работа в Львові');
+INSERT INTO JOB_TEXT  VALUES (3, 'Работа в Дніпрі');
 
 --Написать SQL-запрос, который будет выводить url и description записей, у которых location это Киев или Львов.
 SELECT JOB.url, JOB_TEXT.description FROM JOB, JOB_TEXT
-WHERE JOB.location In ('Киев', 'Львов') and JOB_TEXT.id_job = JOB.id;
+WHERE JOB.location In ('Київ', 'Львів') and JOB_TEXT.id_job = JOB.id;
 
